0831ad58dd7b11b1f7400fe29fba28a7af059697
Reehaz_sherstha <97822084+ReehaStha@users.noreply.github.com>
2022-11-21 21:00:13 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "demobot86@gmail.com"  # Enter your address
+receiver_email = input('Enter The Reciver Email: ')  # Enter receiver address
+password = 'yskncchmyioqotdj'
+message = """\
+Subject: E-Mail Send By Python
+
+This message is sent from Reehaz
+    By Automatic Email Sender
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    print('Login Successfull.')
+    server.sendmail(sender_email, receiver_email, message)
+    print('E-mail send Successfully.')
